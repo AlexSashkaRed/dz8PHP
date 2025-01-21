da36fe53e133089307f35595dc4fcf0473fab617
AlexSashkaRed <chervonyaa@gmail.com>
2025-01-21 21:18:26 +0300

    Update htmlVars.php
    Update htmlVars.php

--- htmlVars.php
+++ htmlVars.php
@@ -9,15 +9,7 @@ $htmHead = "<!DOCTYPE HTML>
     Некая информация обо мне как об авторе:<br>
     'Нормальный автор, талантливый'<br><br>";
 $htmlEnd = "
-    <h2>
-        <ul>
-            <?php  header('Location: ./dz7/index.php'); ?>
-            <li><a href = 'dz6file1.php'>Файл отсутствует</a></li>
-            <li><a href = 'dz6file2.php?text=какой-то+случайный+текст'>Сохранить файл</a></li>
-            <li><a href = 'dz6file3.php'>Страница счетчика</a></li>
-            <li><a href = 'dz6file4.php'>КСтарница отображения</a></li>
-        </ul>
-    </h2>
+ 
 </body>
 </html>
 ";
